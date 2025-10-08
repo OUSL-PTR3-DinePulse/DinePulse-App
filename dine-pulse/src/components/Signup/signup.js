4447c0af47090f0a024b7212c50d00257276be76
Vihaga Manodya <s22010391@ousl.lk>
2025-10-08 16:33:22 +0530

    create component files

---
+++ dine-pulse/src/components/Signup/signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './signup.css'
+
+const signup = () => {
+  return (
+    <div className='signupContainer'>
+      vihaga
+    </div>
+  )
+}
+
+export default signup
